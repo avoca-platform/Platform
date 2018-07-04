a15b6e3f90df2069e25fcefd92bdad7cb458c02c
Jacky <tdhungit@gmail.com>
2018-07-04 14:13:04 +0700

    fixed title

--- imports/common/Translation.js
+++ imports/common/Translation.js
@@ -97,7 +97,7 @@ export class PT extends Component {
             settings[key] = setting;
         }
 
-        let pageTitle = settings && ((settings['Systems:title'] && settings['Systems:title'].value) || 'Hana Platform');
+        let pageTitle = settings && ((settings['Systems:title'] && settings['Systems:title'].value) || 'Avoca Platform');
 
         if (title) {
             pageTitle = title + ' | ' + pageTitle;
